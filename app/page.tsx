ddf32e2b31bd4b5f60118e7899da9c1725693671
AatjeD <101330944+AatjeD@users.noreply.github.com>
2024-09-03 01:51:42 +0200

    error fixen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+'use client';
 
+import React, { useEffect, useState } from 'react';
 import Layout from './layout';
 import HeroSection from '../components/HeroSection';
+import TopEventSection from '../components/TopEventSection';
+import './globals.css';
+
 export default function HomePage() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Layout>
-      <div>
-
-        <HeroSection/>
-        
+      <div className="bodyContainer">
+        <HeroSection />
+        <TopEventSection />
       </div>
     </Layout>
   );
